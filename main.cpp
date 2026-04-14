ba3e944282e96dd411a08215d3500bdb9507b0af
Dhanush H V <dhanushhv75@gmail.com>
2026-04-14 19:36:21 +0000

    Azure rsync

--- main.cpp
+++ main.cpp
@@ -1,55 +1,63 @@
-#include <iostream>
-#include <string>
 #include "main.h"
+#include <iostream>
 
 const int tea_party(const int tea, const int candy) {
-    return (tea < 5 || candy < 5) ? 0 : (tea >= candy * 2 || candy >= tea * 2) ? 2 : 1;
+  return (tea < 5 || candy < 5)                   ? 0
+         : (tea >= candy * 2 || candy >= tea * 2) ? 2
+                                                  : 1;
 }
 
 const std::string fizz_string(const std::string str) {
-    int size = str.size();
+  int size = str.size();
 
-    if (str[0] == 'f' && str[size-1] == 'b') return "FizzBuzz";
-    if (str[0] == 'f') return "Fizz";
-    if (str[size-1] == 'b') return "Buzz";
+  if (str[0] == 'f' && str[size - 1] == 'b')
+    return "FizzBuzz";
+  if (str[0] == 'f')
+    return "Fizz";
+  if (str[size - 1] == 'b')
+    return "Buzz";
 
-    return str;
+  return str;
 }
 
 const std::string fizz_string2(const int n) {
-    if (n % 3 == 0 && n % 5 == 0) return "FizzBuzz!";
-    if (n % 3 == 0) return "Fizz!";
-    if (n % 5 == 0) return "Buzz!";
+  if (n % 3 == 0 && n % 5 == 0)
+    return "FizzBuzz!";
+  if (n % 3 == 0)
+    return "Fizz!";
+  if (n % 5 == 0)
+    return "Buzz!";
 
-    return std::to_string(n) + "!";
+  return std::to_string(n) + "!";
 }
 
 const bool two_as_one(const int a, const int b, const int c) {
-    return a + b == c || a + c == b || b + c == a;
+  return a + b == c || a + c == b || b + c == a;
 }
 
 #ifndef TESTING
 int main() {
-    std::cout << "\ntea_party:\n";
-    std::cout << (tea_party(6, 8) == 1 ? "OK" : "FAILED") << std::endl;
-    std::cout << (tea_party(3, 8) == 0 ? "OK" : "FAILED") << std::endl;
-    std::cout << (tea_party(20, 6) == 2 ? "OK" : "FAILED") << std::endl;
+  std::cout << "\ntea_party:\n";
+  std::cout << (tea_party(6, 8) == 1 ? "OK" : "FAILED") << std::endl;
+  std::cout << (tea_party(3, 8) == 0 ? "OK" : "FAILED") << std::endl;
+  std::cout << (tea_party(20, 6) == 2 ? "OK" : "FAILED") << std::endl;
 
-    std::cout << "\nfizz_string:\n";
-    std::cout << (fizz_string("fig") == "Fizz" ? "OK" : "FAILED") << std::endl;
-    std::cout << (fizz_string("dib") == "Buzz" ? "OK" : "FAILED") << std::endl;
-    std::cout << (fizz_string("fib") == "FizzBuzz" ? "OK" : "FAILED") << std::endl;
+  std::cout << "\nfizz_string:\n";
+  std::cout << (fizz_string("fig") == "Fizz" ? "OK" : "FAILED") << std::endl;
+  std::cout << (fizz_string("dib") == "Buzz" ? "OK" : "FAILED") << std::endl;
+  std::cout << (fizz_string("fib") == "FizzBuzz" ? "OK" : "FAILED")
+            << std::endl;
 
-    std::cout << "\nfizz_string2:\n";
-    std::cout << (fizz_string2(1) == "1!" ? "OK" : "FAILED") << std::endl;
-    std::cout << (fizz_string2(2) == "2!" ? "OK" : "FAILED") << std::endl;
-    std::cout << (fizz_string2(3) == "Fizz!" ? "OK" : "FAILED") << std::endl;
+  std::cout << "\nfizz_string2:\n";
+  std::cout << (fizz_string2(1) == "1!" ? "OK" : "FAILED") << std::endl;
+  std::cout << (fizz_string2(2) == "2!" ? "OK" : "FAILED") << std::endl;
+  std::cout << (fizz_string2(3) == "Fizz!" ? "OK" : "FAILED") << std::endl;
 
-    std::cout << "\ntwo_as_one:\n";
-    std::cout << (two_as_one(1, 2, 3) ? "OK" : "FAILED") << std::endl;
-    std::cout << (two_as_one(3, 1, 2) ? "OK" : "FAILED") << std::endl;
-    std::cout << (!two_as_one(3, 2, 2) ? "OK" : "FAILED") << std::endl;
+  std::cout << "\ntwo_as_one:\n";
+  std::cout << (two_as_one(1, 2, 3) ? "OK" : "FAILED") << std::endl;
+  std::cout << (two_as_one(3, 1, 2) ? "OK" : "FAILED") << std::endl;
+  std::cout << (!two_as_one(3, 2, 2) ? "OK" : "FAILED") << std::endl;
 
-    return 0;
+  return 0;
 }
 #endif
